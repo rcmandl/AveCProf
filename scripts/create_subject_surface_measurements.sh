c9de4224732a78efa719d9ab03c2d8f0f7fdfc86
rcmandl <r_c_w_mandl@yahoo.com>
2020-12-31 09:10:33 +0100

    overhaul create_subject_surface_measurements.sh: but still have to check if x and y coordinate negation is still necessary

--- scripts/create_subject_surface_measurements.sh
+++ scripts/create_subject_surface_measurements.sh
@@ -9,24 +9,39 @@
 
 
 # set -x
-if [ $# -ne 1 ]
+NPARAM=$#
+if (( NPARAM < 1 || NPARAM > 2 ))
 then
 echo
 echo "wrong number of parameters."
 echo
-echo  "usage: create_subject_surface_measurements <subject's freesurfer directory>"
+echo  "usage: create_subject_surface_measurements <subject's freesurfer directory> [path to label files]"
 echo
 echo ""   
-echo "Creates surface files (pial ad white) that are ready to be used with samplecortex (found in layer directory)"   
+echo "Creates surface files (pial ad white) for given label files, that serve as input for samplecortex"
 echo "These surfaces are in native space (e.g. brain.mgz)"
-echo ""   
+echo
+echo "If no path to label files is provided then te default label files are used (found in label directory of the subject's directory)"
+echo "You can create label files from annotation files using the program mri_annotation2label (part of freesurfer)"
+echo "In this way you can create labels for complete atlases. Note that you have to create these labels before you call this script."
+echo
+echo
 exit 1
 fi
+
 
 CURRENTDIR=`pwd`
 cd $1
 mkdir layers
 cd layers
+
+if (( 2 == NPARAM ))
+then
+LABELPATH=$2
+else
+LABELPATH="../label"
+fi
+
 
 #
 #
@@ -39,26 +54,12 @@ cp ../surf/rh.white .
 cp ../surf/lh.avg_curv .
 cp ../surf/rh.avg_curv .
 
-mris_convert --cras_add --to-scanner lh.pial lh.pial.asc
-mris_convert --cras_add --to-scanner lh.white lh.white.asc
-mris_convert --cras_add --to-scanner rh.pial rh.pial.asc
-mris_convert --cras_add --to-scanner rh.white rh.white.asc
+# get curvature information in ascii format add to coordiates (in native coordinates)
 
-
-# now adjust the files above (remove lines etc)
-
-awk '//{if (NR>2){print $0}}' < lh.pial.asc > lh.pial-2.asc
-awk '//{if (NR>2){print $0}}' < lh.white.asc > lh.white-2.asc
-awk '//{if (NR>2){print $0}}' < rh.pial.asc > rh.pial-2.asc
-awk '//{if (NR>2){print $0}}' < rh.white.asc > rh.white-2.asc
-
-
-# get curvature information in ascii format
-
-mris_convert -c lh.avg_curv lh.pial lh.pial.avg_curv.asc
-mris_convert -c lh.avg_curv lh.white lh.white.avg_curv.asc
-mris_convert -c rh.avg_curv rh.pial rh.pial.avg_curv.asc
-mris_convert -c rh.avg_curv rh.white rh.white.avg_curv.asc
+mris_convert --cras_add --to-scanner -c lh.avg_curv lh.pial lh.pial.avg_curv.asc
+mris_convert --cras_add --to-scanner -c lh.avg_curv lh.white lh.white.avg_curv.asc
+mris_convert --cras_add --to-scanner -c rh.avg_curv rh.pial rh.pial.avg_curv.asc
+mris_convert --cras_add --to-scanner -c rh.avg_curv rh.white rh.white.avg_curv.asc
 
 rm lh.pial lh.white rh.pial rh.white
 
@@ -72,27 +73,29 @@ rm lh.pial lh.white rh.pial rh.white
 # that are part of the particular label.
 
 ##
-## NOTE THAT WE NEGATE THE X AND Y COORDINATES!!
+## NOTE THAT WE NEGATE THE X AND Y COORDINATES!! IS THIS STILL NECESSARY? (CHECK)
 ##
 
 echo "# R script to select points from inner and out surface given their index in a file." > selectPoints_l.R
 echo "# Assuming that the first 2 lines of the label and surface files are removed." >> selectPoints_l.R
 echo "indices <- read.table(\"tmpLabel_l\")" >> selectPoints_l.R
-echo "pial <- read.table(\"lh.pial-2.asc\");" >> selectPoints_l.R
+echo "pial <- read.table(\"lh.pial.avg_curv.asc\");" >> selectPoints_l.R
 echo "i<-indices[,1]" >> selectPoints_l.R
 echo "i=i+1; # indices start at 0 while R starts at 1" >> selectPoints_l.R
-echo "pialSelected<-pial[i,1:3]" >> selectPoints_l.R
+echo "pialSelected<-pial[i,2:4]" >> selectPoints_l.R
+echo "curvatureSelected<-pial[i,5]" >> selectPoints_l.R
 echo "pialSelected[,1]<-pialSelected[,1]*-1" >> selectPoints_l.R
 echo "pialSelected[,2]<-pialSelected[,2]*-1" >> selectPoints_l.R
 echo "write.table(pialSelected, file=\"PIALOUTFILE_l\",col.names=FALSE, row.names=FALSE)"  >> selectPoints_l.R
-echo "wmSelected<-pial[i,1:3]" >> selectPoints_l.R
+echo "write.table(curvatureSelected, file=\"CURVATUREOUTFILE_l\")" >> selectPoints_l.R
+echo "wmSelected<-pial[i,2:4]" >> selectPoints_l.R
 echo "wmSelected[,1]<-pialSelected[,1]*-1" >> selectPoints_l.R
 echo "wmSelected[,2]<-pialSelected[,2]*-1" >> selectPoints_l.R
 echo "write.table(wmSelected, file=\"WHITEOUTFILE_l\",col.names=FALSE, row.names=FALSE)" >> selectPoints_l.R
 
 # Now for each label file in the label directory do:
 
-for x in `ls ../label/lh.*.label`
+for x in `ls ${LABELPATH}/lh.*.label`
 do
    # first line removes the two first lines because they contain header info,
    awk '//{if (NR>2){print $0}}' < ${x} > tmpLabel_l
@@ -100,6 +103,7 @@ do
    BASE=`basename $x .label`
    mv PIALOUTFILE_l ${BASE}.pial
    mv WHITEOUTFILE_l ${BASE}.white
+   mv CURVATUREOUTFILE_l ${BASE}.curvature
    rm tmpLabel_l
 done
 
@@ -120,21 +124,23 @@ rm selectPoints_l.R
 echo "# R script to select points from inner and out surface given their index in a file." > selectPoints_r.R
 echo "# Assuming that the first 2 lines of the label and surface files are removed." >> selectPoints_r.R
 echo "indices <- read.table(\"tmpLabel_r\")" >> selectPoints_r.R
-echo "pial <- read.table(\"rh.pial-2.asc\");" >> selectPoints_r.R
+echo "pial <- read.table(\"rh.pial.avg_curv.asc\");" >> selectPoints_r.R
 echo "i<-indices[,1]" >> selectPoints_r.R
 echo "i=i+1; # indices start at 0 while R starts at 1" >> selectPoints_r.R
-echo "pialSelected<-pial[i,1:3]" >> selectPoints_r.R
+echo "pialSelected<-pial[i,2:4]" >> selectPoints_r.R
+echo "curvatureSelected<-pial[i,5]" >> selectPoints_r.R
 echo "pialSelected[,1]<-pialSelected[,1]*-1" >> selectPoints_r.R
 echo "pialSelected[,2]<-pialSelected[,2]*-1" >> selectPoints_r.R
 echo "write.table(pialSelected, file=\"PIALOUTFILE_r\",col.names=FALSE, row.names=FALSE)"  >> selectPoints_r.R
-echo "wmSelected<-pial[i,1:3]" >> selectPoints_r.R
+echo "write.table(curvatureSelected, file=\"CURVATUREOUTFILE_r\")" >> selectPoints_r.R
+echo "wmSelected<-pial[i,2:4]" >> selectPoints_r.R
 echo "wmSelected[,1]<-pialSelected[,1]*-1" >> selectPoints_r.R
 echo "wmSelected[,2]<-pialSelected[,2]*-1" >> selectPoints_r.R
 echo "write.table(wmSelected, file=\"WHITEOUTFILE_r\",col.names=FALSE, row.names=FALSE)" >> selectPoints_r.R
 
 # Now for each label file in the label directory do:
 
-for x in `ls ../label/rh.*.label`
+for x in `ls ${LABELPATH}/rh.*.label`
 do
    # first line removes the two first lines because they contain header info,
    awk '//{if (NR>2){print $0}}' < ${x} > tmpLabel_r
@@ -142,10 +148,11 @@ do
    BASE=`basename $x .label`
    mv PIALOUTFILE_r ${BASE}.pial
    mv WHITEOUTFILE_r ${BASE}.white
+   mv CURVATUREOUTFILE_r ${BASE}.curvature
    rm tmpLabel_r
 done
 
-rm selectPoints_r.R
+rm selectPoints_r.R lh.pial.avg_curv.asc lh.white.avg_curv.asc rh.pial.avg_curv.asc rh.white.avg_curv.asc
 
 # and return back to original dir 
 cd $CURRENTDIR
