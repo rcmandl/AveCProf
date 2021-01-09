10f87fccae17b5b2b96cc2ca085ef0a38e10e727
rcmandl <r_c_w_mandl@yahoo.com>
2021-01-09 17:02:18 +0100

    small adjustments itkDeconvolution

--- C++/deconvolution/itkDeconvolution.cxx
+++ C++/deconvolution/itkDeconvolution.cxx
@@ -26,6 +26,7 @@
 #include "itkRichardsonLucyDeconvolutionImageFilter.h"
 #include "itkWienerDeconvolutionImageFilter.h"
 #include "itkParametricBlindLeastSquaresDeconvolutionImageFilter.h"
+#include "itkTikhonovDeconvolutionImageFilter.h"
 #include "itkCastImageFilter.h"
 #include "itkMacro.h"
 #include "itkMath.h"
@@ -68,7 +69,7 @@ int main(int argc, char* argv[])
     command.SetOption("noNormalize","n", false, "do not normalize the deconvolution kernel (default is normalize)");
     command.SetOptionLongTag("noNormalize","noNormalizeKernel");
     
-    command.SetOption("subsampleFactor","s", false, "subsample factor (x y z) fo input volume");
+    command.SetOption("subsampleFactor","s", false, "subsample factor (x y z) fo input volume (should be integer and 1 or larger)");
     command.SetOptionLongTag("subsampleFactor","subsampleFactor");
     command.AddOptionField("subsampleFactor", "SX", MetaCommand::INT, false,"1");
     command.AddOptionField("subsampleFactor", "SY", MetaCommand::INT, false,"1");
@@ -82,7 +83,7 @@ int main(int argc, char* argv[])
     command.SetOptionLongTag("Alpha","alpha");
     command.AddOptionField("Alpha", "alpha", MetaCommand::FLOAT, true,"0.05");
     
-    command.SetOption("DeconvolutionType","t", false,"Deconvolution type: 1 = Landweber; 2 = projected Landweber; 3 = Richardson Lucy; 4 = Wiener; 5 = parametric blind least squares (FOR NOW ONLY '1' IS IMPLEMENTED) ");
+    command.SetOption("DeconvolutionType","t", false,"Deconvolution type: 1 = Landweber; 2 = projected Landweber; 3 = Richardson Lucy; 4 = Wiener; 5 = Tikhonov");
     command.SetOptionLongTag("DeconvolutionType","deconvolutionType");
     command.AddOptionField("DeconvolutionType", "DeconvType", MetaCommand::INT, false,"1");
     
@@ -92,7 +93,7 @@ int main(int argc, char* argv[])
         std::cerr << "With this program you can upsample a given 3D volume of type float and apply deconvolution to increase detail." << std::endl;
         std::cerr << std::endl;
         std::cerr << "USAGE:" << std::endl;
-        std::cerr << "itkLandweberDeconvolution <inputVolume>.nii <kernel>.nii <outputVolume>.nii" << std::endl;
+        std::cerr << "itkDeconvolution <inputVolume>.nii <kernel>.nii <outputVolume>.nii" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -108,16 +109,45 @@ int main(int argc, char* argv[])
     float alpha            = command.GetValueAsFloat("Alpha","alpha");
     int deconvolutionType  = command.GetValueAsInt("DeconvolutionType","DeconvType");
     
+    if (subsampleFactorX < 1 || subsampleFactorY < 1 || subsampleFactorZ < 1) {
+        std::cerr << std::endl;
+        std::cerr << std::endl;
+        std::cerr << "ERROR: subsample factors should be integer and equal or larger than 1" << std::endl;
+        std::cerr << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    if ( deconvolutionType < 1 || deconvolutionType > 5 ) {
+        std::cerr << std::endl;
+        std::cerr << std::endl;
+        std::cerr << "ERROR: deconvolution type should be 1-5" << std::endl;
+        std::cerr << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    if ( nrOfIterations < 0 ) {
+        std::cerr << std::endl;
+        std::cerr << std::endl;
+        std::cerr << "ERROR: nr of iterations should be equal or larger than zero" << std::endl;
+        std::cerr << std::endl;
+        return EXIT_FAILURE;
+    }
     
     // EOF command parsing
     
     const unsigned int Dimension = 3;
     
-    using inputVoxelType = float; // for now only operate on float images
-    using ImageType = itk::Image<inputVoxelType, Dimension>;
+    using inputVoxelTypeFloat = float;
+    using inputVoxelTypeInt = int;
+    
+    using ImageType = itk::Image<inputVoxelTypeFloat, Dimension>;
     using WriterType = itk::ImageFileWriter<ImageType>;
+    
     using LandWeberDeconvolutionFilterType = itk::LandweberDeconvolutionImageFilter< ImageType >;
-    using DeconvolutionFilterType = itk::LandweberDeconvolutionImageFilter< ImageType >;
+    using ProjectedLandweberDeconvolutionFilterType = itk::ProjectedLandweberDeconvolutionImageFilter< ImageType >;
+    using RichardsonLucyDeconvolutionFilterType = itk::RichardsonLucyDeconvolutionImageFilter< ImageType >;
+    using WienerDeconvolutionFilterType = itk::WienerDeconvolutionImageFilter< ImageType >;
+    using TikhonovDeconvolutionImageFilterType = itk::TikhonovDeconvolutionImageFilter< ImageType >;
     
     itk::ImageIOBase::Pointer volumeIO = getImageIO(inFileName);
     itk::ImageIOBase::Pointer filterIO = getImageIO(kernelFileName);
@@ -162,41 +192,145 @@ int main(int argc, char* argv[])
     in_filterReader->SetFileName(filterIO->GetFileName());
     in_filterReader->Update();
 
-    // To implement if necessary: different types of the deconvolution algorithm
-    // if (1 == deconvolutionType) {DeconvolutionFilterType::Pointer deconvolutionFilter = LandWeberDeconvolutionFilterType::New();}
-    // if (2 == deconvolutionType) {DeconvolutionFilterType::Pointer deconvolutionFilter ProjectedLandWeberDeconvolutionFilterType::New();}
-    // if (3 == deconvolutionType) {DeconvolutionFilterType::Pointer deconvolutionFilter = RichardsonLucyDeconvolutionFilterType::New();}
-    // if (4 == deconvolutionType) {DeconvolutionFilterType::Pointer deconvolutionFilter = WienerDeconvolutionFilterType::New();}
-    // if (5 == deconvolutionType) {DeconvolutionFilterType::Pointer deconvolutionFilter = ParametricBlindLeastSquaresDeconvolutionFilterType::New();}
+    
+    LandWeberDeconvolutionFilterType::Pointer LandWeberDeconvolutionFilter = LandWeberDeconvolutionFilterType::New();
+    ProjectedLandweberDeconvolutionFilterType::Pointer ProjectedLandWeberDeconvolutionFilter = ProjectedLandweberDeconvolutionFilterType::New();
+    RichardsonLucyDeconvolutionFilterType::Pointer RichardsonLucyDeconvolutionFilter = RichardsonLucyDeconvolutionFilterType::New();
+    WienerDeconvolutionFilterType::Pointer WienerDeconvolutionFilter = WienerDeconvolutionFilterType::New();
+    TikhonovDeconvolutionImageFilterType::Pointer TikhonovDeconvolutionFilter = TikhonovDeconvolutionImageFilterType::New();
+    
+    switch (deconvolutionType) {
+        case 1:
+            LandWeberDeconvolutionFilter->SetInput(resampler->GetOutput());
+            LandWeberDeconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
+            
+            LandWeberDeconvolutionFilter->NormalizeOn();
+            if (noNormalizeKernel)LandWeberDeconvolutionFilter->NormalizeOff();
+            
+            LandWeberDeconvolutionFilter->SetAlpha(alpha);
+            
+            if (itk::Math::NotExactlyEquals(LandWeberDeconvolutionFilter->GetAlpha(), alpha)) {
+                std::cerr << "Set/GetAlpha() test failed." << std::endl;
+                return EXIT_FAILURE;
+            }
 
-    // the statement below will be replaced by the option for the various different filter types
-    DeconvolutionFilterType::Pointer deconvolutionFilter = DeconvolutionFilterType::New();
-    
-    deconvolutionFilter->SetInput(resampler->GetOutput());
-    deconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
-    
-    deconvolutionFilter->NormalizeOn();
-    if (noNormalizeKernel)deconvolutionFilter->NormalizeOff();
-    
-    deconvolutionFilter->SetAlpha(alpha);
-    
-    if (itk::Math::NotExactlyEquals(deconvolutionFilter->GetAlpha(), alpha)) {
-        std::cerr << "Set/GetAlpha() test failed." << std::endl;
-        return EXIT_FAILURE;
+            LandWeberDeconvolutionFilter->SetNumberOfIterations(nrOfIterations);
+            LandWeberDeconvolutionFilter->SetOutputRegionModeToSame();
+            LandWeberDeconvolutionFilter->Update();
+            
+            // Write out the deconvolution result
+            try {
+                WriterType::Pointer writer = WriterType::New();
+                writer->SetFileName(outFileName);
+                writer->SetInput(LandWeberDeconvolutionFilter->GetOutput());
+                writer->Update();
+            } catch ( itk::ExceptionObject & e ) {
+                std::cerr << "Unexpected exception caught when writing deconvoluted image: " << std::endl;
+                return EXIT_FAILURE;
+            }
+            break;
+            
+        case 2:
+            ProjectedLandWeberDeconvolutionFilter->SetInput(resampler->GetOutput());
+            ProjectedLandWeberDeconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
+            
+            ProjectedLandWeberDeconvolutionFilter->NormalizeOn();
+            if (noNormalizeKernel)ProjectedLandWeberDeconvolutionFilter->NormalizeOff();
+            
+            ProjectedLandWeberDeconvolutionFilter->SetAlpha(alpha);
+            
+            if (itk::Math::NotExactlyEquals(ProjectedLandWeberDeconvolutionFilter->GetAlpha(), alpha)) {
+                std::cerr << "Set/GetAlpha() test failed." << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            ProjectedLandWeberDeconvolutionFilter->SetNumberOfIterations(nrOfIterations);
+            ProjectedLandWeberDeconvolutionFilter->SetOutputRegionModeToSame();
+            ProjectedLandWeberDeconvolutionFilter->Update();
+            
+            // Write out the deconvolution result
+            try {
+                WriterType::Pointer writer = WriterType::New();
+                writer->SetFileName(outFileName);
+                writer->SetInput(ProjectedLandWeberDeconvolutionFilter->GetOutput());
+                writer->Update();
+            } catch ( itk::ExceptionObject & e ) {
+                std::cerr << "Unexpected exception caught when writing deconvoluted image: " << std::endl;
+                return EXIT_FAILURE;
+            }
+            break;
+        case 3:
+            RichardsonLucyDeconvolutionFilter->SetInput(resampler->GetOutput());
+            RichardsonLucyDeconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
+            
+            RichardsonLucyDeconvolutionFilter->NormalizeOn();
+            if (noNormalizeKernel)RichardsonLucyDeconvolutionFilter->NormalizeOff();
+
+            RichardsonLucyDeconvolutionFilter->SetNumberOfIterations(nrOfIterations);
+            RichardsonLucyDeconvolutionFilter->SetOutputRegionModeToSame();
+            RichardsonLucyDeconvolutionFilter->Update();
+            
+            // Write out the deconvolution result
+            try {
+                WriterType::Pointer writer = WriterType::New();
+                writer->SetFileName(outFileName);
+                writer->SetInput(RichardsonLucyDeconvolutionFilter->GetOutput());
+                writer->Update();
+            } catch ( itk::ExceptionObject & e ) {
+                std::cerr << "Unexpected exception caught when writing deconvoluted image: " << std::endl;
+                return EXIT_FAILURE;
+            }
+            break;
+        case 4:
+            WienerDeconvolutionFilter->SetInput(resampler->GetOutput());
+            WienerDeconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
+            
+            WienerDeconvolutionFilter->NormalizeOn();
+            if (noNormalizeKernel)WienerDeconvolutionFilter->NormalizeOff();
+            
+            WienerDeconvolutionFilter->SetOutputRegionModeToSame();
+            WienerDeconvolutionFilter->Update();
+            
+            // Write out the deconvolution result
+            try {
+                WriterType::Pointer writer = WriterType::New();
+                writer->SetFileName(outFileName);
+                writer->SetInput(WienerDeconvolutionFilter->GetOutput());
+                writer->Update();
+            } catch ( itk::ExceptionObject & e ) {
+                std::cerr << "Unexpected exception caught when writing deconvoluted image: " << std::endl;
+                return EXIT_FAILURE;
+            }
+            break;
+            
+        case 5:
+            TikhonovDeconvolutionFilter->SetInput(resampler->GetOutput());
+            TikhonovDeconvolutionFilter->SetKernelImage(in_filterReader->GetOutput());
+            
+            TikhonovDeconvolutionFilter->NormalizeOn();
+            if (noNormalizeKernel) TikhonovDeconvolutionFilter->NormalizeOff();
+            
+            TikhonovDeconvolutionFilter->SetOutputRegionModeToSame();
+            TikhonovDeconvolutionFilter->Update();
+            
+            // Write out the deconvolution result
+            try {
+                WriterType::Pointer writer = WriterType::New();
+                writer->SetFileName(outFileName);
+                writer->SetInput(TikhonovDeconvolutionFilter->GetOutput());
+                writer->Update();
+            } catch ( itk::ExceptionObject & e ) {
+                std::cerr << "Unexpected exception caught when writing deconvoluted image: " << std::endl;
+                return EXIT_FAILURE;
+            }
+            break;
+        default:
+            std::cerr << std::endl << std::endl;
+            std::cerr << "Unknown dconvolution type!" << std::endl;
+            return EXIT_FAILURE;
+            break;
     }
 
-    deconvolutionFilter->SetNumberOfIterations(nrOfIterations);
-
-    // Write out the deconvolution result
-    try {
-        WriterType::Pointer writer = WriterType::New();
-        writer->SetFileName(outFileName);
-        writer->SetInput(deconvolutionFilter->GetOutput());
-        writer->Update();
-    } catch ( itk::ExceptionObject & e ) {
-        std::cerr << "Unexpected exception caught when writing deconvolution image: " << std::endl;
-        return EXIT_FAILURE;
-    }
-
+    
     return EXIT_SUCCESS;
 }
